a5d9a56fed2e9d55f4748e807abe84eff1f4456c
Tho^ng Nguyen <xcodevn@gmail.com>
2013-09-10 23:38:35 +0200

    Fix a bug in LeonTestSuite cause by wrong compute std-dev :)

--- lemmafilterTests.sh
+++ lemmafilterTests.sh
@@ -2,4 +2,10 @@
 
 # Run testcases for lemma filter only
 # This below command is very useful (just REMEMBER to design testcases BEFORE coding)
-sbt "test-only leon.test.solvers.lemmafilter.*"
+{ 
+while true; do
+	sbt "test-only leon.test.solvers.lemmafilter.*"
+	echo "[info] Sleep in two minutes and then wake up!"
+	sleep 120
+done
+}  > log.txt &
